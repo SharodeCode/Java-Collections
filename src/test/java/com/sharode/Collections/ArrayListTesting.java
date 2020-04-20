369f9e1e4b3ad2e5e05b2482ba66e04533795313
SharodeCode <jacob.mcintyre@sharode.com>
2020-04-20 13:04:09 +1000

    Added: Tests for size, capacity

--- src/test/java/com/sharode/Collections/ArrayListTesting.java
+++ src/test/java/com/sharode/Collections/ArrayListTesting.java
@@ -14,8 +14,8 @@ class ArrayListTesting {
 	private Random random = new Random();
 	
 	@Test
-	@DisplayName("Test add single value to ArrayList.")
-	public void ArrayListTest_Add_001()
+	@DisplayName("Test add  single value to ArrayList.")
+	public void ArrayListTest_AddAndGet_001()
 	{
 		ArrayList<Integer> arrayList = new ArrayList<Integer>();
 		
@@ -28,7 +28,7 @@ class ArrayListTesting {
 	
 	@Test
 	@DisplayName("Test add multiple values to ArrayList.")
-	public void ArrayListTest_Add_002()
+	public void ArrayListTest_AddAndGet_002()
 	{
 		ArrayList<Integer> arrayList = new ArrayList<Integer>();
 		
@@ -45,7 +45,7 @@ class ArrayListTesting {
 	
 	@Test
 	@DisplayName("Test add values  more than capacity to test resize of ArrayList.")
-	public void ArrayListTest_Add_003()
+	public void ArrayListTest_AddAndGet_003()
 	{
 		ArrayList<Integer> arrayList = new ArrayList<Integer>();
 		
@@ -63,7 +63,7 @@ class ArrayListTesting {
 	
 	@Test
 	@DisplayName("Test get value that is outside of array bounds.")
-	public void ArrayListTest_Get_001()
+	public void ArrayListTest_AddAndGet_004()
 	{
 		ArrayList<Integer> arrayList = new ArrayList<Integer>();
 		
@@ -79,19 +79,6 @@ class ArrayListTesting {
 		catch (IndexOutOfBoundsException e) {
 			result = true;
 		}
-		
-		assertTrue(result);
-	}
-	
-	@Test
-	@DisplayName("Test value using get, after add.")
-	public void ArrayListTest_AddAndGet_005()
-	{
-		ArrayList<Integer> arrayList = new ArrayList<Integer>();
-		
-		boolean result = false;
-		
-		int a = arrayList.get(5);
 		
 		assertTrue(result);
 	}
@@ -138,6 +125,137 @@ class ArrayListTesting {
 		for(int i = 0; i < randomIntegersTwo.length; i++) {
 			assertTrue(arrayList.get(i) == randomIntegersTwo[i]);
 		}
+	}
+	
+	@Test
+	@DisplayName("Test default capacity.")
+	public void ArrayListTest_Capacity_001()
+	{
+		ArrayList<Integer> arrayList = new ArrayList<Integer>();
+		
+		boolean result = false;
+		
+		result = arrayList.capacity() == 10 ? true : false;
+		
+	    assertTrue(result);
+	}
+	
+	@Test
+	@DisplayName("Test capacity on initialisation.")
+	public void ArrayListTest_Capacity_002()
+	{
+		ArrayList<Integer> arrayList = new ArrayList<Integer>(47);
+		
+		boolean result = false;
+		
+		result = arrayList.capacity() == 47 ? true : false;
+		
+	    assertTrue(result);
+	}
+	
+	@Test
+	@DisplayName("Test capacity after exceeded.")
+	public void ArrayListTest_Capacity_003()
+	{
+		ArrayList<Integer> arrayList = new ArrayList<Integer>();
+		
+		boolean result = false;
+		
+		int initCapacity = (arrayList.capacity() + 1);
+		
+		for(int i = 0; i < initCapacity; i++) {
+			arrayList.add(random.nextInt());
+		}
+		
+		result = arrayList.capacity() == 20 ? true : false;
+		
+	    assertTrue(result);
+	}
+	
+	@Test
+	@DisplayName("Test capacity after clear.")
+	public void ArrayListTest_Capacity_004()
+	{
+		ArrayList<Integer> arrayList = new ArrayList<Integer>();
+		
+		boolean result = false;
+		
+		int initCapacity = (arrayList.capacity() + 1);
+		
+		for(int i = 0; i < initCapacity; i++) {
+			arrayList.add(random.nextInt());
+		}
+		
+		arrayList.clear();
+		
+		result = arrayList.capacity() == 10 ? true : false;
+		
+	    assertTrue(result);
+	}
+	
+	@Test
+	@DisplayName("Test default size.")
+	public void ArrayListTest_Size_001()
+	{
+		ArrayList<Integer> arrayList = new ArrayList<Integer>();
+		
+		boolean result = false;
+		
+		result = arrayList.size() == 0 ? true : false;
+		
+	    assertTrue(result);
+	}
+	
+	@Test
+	@DisplayName("Test size after add.")
+	public void ArrayListTest_Size_002()
+	{
+		ArrayList<Integer> arrayList = new ArrayList<Integer>();
+		
+		boolean result = false;
+		
+		arrayList.add(random.nextInt());
+		
+		result = arrayList.size() == 1 ? true : false;
+		
+	    assertTrue(result);
+	}
+	
+	@Test
+	@DisplayName("Test size after set.")
+	public void ArrayListTest_Size_003()
+	{
+		ArrayList<Integer> arrayList = new ArrayList<Integer>();
+		
+		boolean result = false;
+		
+		arrayList.add(random.nextInt());
+		arrayList.add(random.nextInt());
+		arrayList.add(random.nextInt());
+		
+		arrayList.set(1, random.nextInt());
+		
+		result = arrayList.size() == 3 ? true : false;
+		
+	    assertTrue(result);
+	}
+	
+	@Test
+	@DisplayName("Test size after clear.")
+	public void ArrayListTest_Size_004()
+	{
+		ArrayList<Integer> arrayList = new ArrayList<Integer>();
+		
+		boolean result = false;
+		
+		arrayList.add(random.nextInt());
+		arrayList.add(random.nextInt());
+		arrayList.add(random.nextInt());
+		arrayList.clear();
+		
+		result = arrayList.size() == 0 ? true : false;
+		
+	    assertTrue(result);
 	}
 	
 }
